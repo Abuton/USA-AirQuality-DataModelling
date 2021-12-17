83597579f5bc9b1d7d28d97a7b94a233b281953b
Abuton <alaroabubakarolayemi@yahoo.com>
2021-12-17 13:10:36 +0100

    added modular function and logging

--- task1/crop.py
+++ task1/crop.py
@@ -1,13 +1,18 @@
 # Crop the file to delete  any records before 00:00 1 jan 2010 (1262304000).
+import pandas as pd
+import logging
 
-def crop_data(df):
-    print("Total record Before cropping is: ", df.shape)
-    new_df = df[df['Date Time'] >= '2010-01-01T00:00:00+00:00']
-    print("Total record After cropping is: ", new_df.shape)
+logging.basicConfig(format='%(asctime)s %(levelname)s - Crop data - %(message)s', level=logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
+
+
+def crop_data(df: pd.DataFrame, date_value: str = '2010-01-01T00:00:00+00:00'):
+    logging.info(f"Total record Before cropping is: {df.shape}")
+    new_df = df[df['Date Time'] >= date_value]
+    logging.info(f"Total record After cropping is: {new_df.shape}")
     return new_df
 
 
 if __name__ == "__main__":
-    import pandas as pd
-    df = pd.read_csv("../bristol-air-quality-data.csv", sep=';')
+    df = pd.read_csv("../data/bristol-air-quality-data.csv", sep=';')
     new_df = crop_data(df)
