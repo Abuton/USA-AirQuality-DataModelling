14d455f9417b22f6e2d24d6458b8a7e800fc43c2
Abuton <alaroabubakarolayemi@yahoo.com>
2021-12-17 21:52:31 +0100

    integrated MongoDB for NOSQL data models

--- task5/populate_nosql.py
+++ task5/populate_nosql.py
@@ -1,19 +1,28 @@
 from pymongo import MongoClient
 import pandas as pd
+import logging
+import json
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s - NoSQL PopulateDB - %(message)s",
+    level=logging.INFO,
+)
+logging.getLogger().setLevel(logging.INFO)
 
 
 def connectMongo():
     try:
         conn = MongoClient()
-        print("Connected Succesfully!")
+        logging.info("Connected Succesfully!")
         return conn
     except Exception as e:
-        print("An Error Occured", e)
+        logging.error(f"An Error Occured {e}")
 
 
 def csv_to_json(filename, header=None):
-    data = pd.read_csv(filename, header=header)
-    return data.to_dict('records')
+    data = pd.read_csv(filename, header=header, low_memory=False)
+    data.fillna(0, inplace=True)
+    return json.loads(data.to_json(orient="records"))
 
 
 if __name__ == "__main__":
@@ -22,18 +31,20 @@ if __name__ == "__main__":
     conn = connectMongo()
     # connect to the database
     db = conn[database_name]
-
     # create a new table
     collection = db[table_name]
 
     records = csv_to_json("../data/Brislington_Depot.csv")
-    recs = records[:10]
-    try:
-        # inserting the data in the database
-        collection.insert_many(recs)
-        print("Records inserted")
-    except Exception as e:
-        print("Error Occured", e)
+    # recs = records[:10]
+    if collection.count_documents({}) > 0:
+        logging.info("Removing documents from db")
+    else:
+        try:
+            # inserting the data in the database
+            collection.insert_many(records)
+            print("Records inserted")
+        except Exception as e:
+            print("Error Occured", e)
 
 # for i in collection.find({'title': 'MongoDB and Python'}):
 #     print(i)
