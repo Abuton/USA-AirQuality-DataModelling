fee66874b45d8b184f20d6e492c98ca7ffe33ee5
Abuton <alaroabubakarolayemi@yahoo.com>
2021-12-17 16:38:56 +0100

    added test_clean module

---
+++ tests/test_clean.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import unittest
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join("../")))
+
+from task1.clean import filter_data
+
+
+class TestCleanData(unittest.TestCase):
+    """
+    A class for unit-testing all functions in the clean.py file
+
+    Args:
+    -----
+    unittest.TestCase this allows the new class to inherit
+    from the unittest module
+    """
+
+    def setUp(self) -> pd.DataFrame:
+        self.df = pd.read_csv("../data/sample_test_data.csv")
+
+    def test_filter_data(self):
+        column_name = "SiteID"
+        df = filter_data(self.df)
+        missing_count = df[column_name].isna().sum()
+        self.assertEqual(missing_count, 0, "Value should be zero")
+
+
+if __name__ == "__main__":
+    unittest.main()
