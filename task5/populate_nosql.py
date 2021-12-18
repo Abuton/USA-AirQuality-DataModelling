6ef4aefaa8e8a42fbe942410a46d1ab3c43396ce
Abuton <alaroabubakarolayemi@yahoo.com>
2021-12-18 16:55:07 +0100

    fixed mongodb insert_many error

--- task5/populate_nosql.py
+++ task5/populate_nosql.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 import pandas as pd
 import logging
-import json
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)s - NoSQL PopulateDB - %(message)s",
@@ -19,32 +18,41 @@ def connectMongo():
         logging.error(f"An Error Occured {e}")
 
 
-def csv_to_json(filename, header=None):
-    data = pd.read_csv(filename, header=header, low_memory=False)
-    data.fillna(0, inplace=True)
-    return json.loads(data.to_json(orient="records"))
+def csv_to_json(filename: str):
+    data = pd.read_csv(filename, low_memory=False)
+    # data.fillna(0, inplace=True)
+    return data.to_dict(orient="records")
+
+
+def insert_into_mongodb(filename: str = "../data/clean_Brislington_Depot.csv"):
+    records = csv_to_json(filename)
+    collection.insert_many(records)
+    logging.info("Records inserted Successfully:")
 
 
 if __name__ == "__main__":
     database_name = "pollution_nosql"
     table_name = "BrislingtonDepot"
+
     conn = connectMongo()
     # connect to the database
     db = conn[database_name]
     # create a new table
     collection = db[table_name]
 
-    records = csv_to_json("../data/Brislington_Depot.csv")
-    # recs = records[:10]
     if collection.count_documents({}) > 0:
-        logging.info("Removing documents from db")
+        count = collection.delete_many({})
+        logging.info(f"{count} documents deleted.")
+        insert_into_mongodb()
     else:
         try:
-            # inserting the data in the database
-            collection.insert_many(records)
-            print("Records inserted")
+            insert_into_mongodb()
         except Exception as e:
-            print("Error Occured", e)
+            logging.error(f"Error Occured {e}")
+
+    logging.info(f"Total Documents = {collection.count_documents({})}")
+
+# db.BrislingtonDepot.find( {NOx: {$gt: 30 } } )
 
 # for i in collection.find({'title': 'MongoDB and Python'}):
 #     print(i)
